2f830c13f18e644252c81473dc01812af9f69092
Mohamed-A-I/ <mimo.007007@gmail.com>
2021-06-02 23:14:41 +0200

    add clicked method

--- code/main.py
+++ code/main.py
@@ -9,30 +9,45 @@ root.title("manacala game")
 button_width=15
 p1_pos=0
 p2_pos=200
-rec = []
-for i in range(14):
-    if(i == 6 or i==13):
-        rec.insert(i,0)
-        continue
-    rec.insert(i,4)
-tkinter.Label(root,text='score \n {}'.format(rec[13]),bg='blue',width=18,height=23).place(x=0,y=0)
+binAmount = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
 
-tkinter.Button(root,text=rec[0],bg='red',width=button_width,height=10,borderwidth=3).place(x=150,y=p2_pos)
-tkinter.Button(root,text=rec[1],bg='red',width=button_width,height=10,borderwidth=3).place(x=300,y=p2_pos)
-tkinter.Button(root,text=rec[2],bg='red',width=button_width,height=10,borderwidth=3).place(x=450,y=p2_pos)
-tkinter.Button(root,text=rec[3],bg='red',width=button_width,height=10,borderwidth=3).place(x=600,y=p2_pos)
-tkinter.Button(root,text=rec[4],bg='red',width=button_width,height=10,borderwidth=3).place(x=750,y=p2_pos)
-tkinter.Button(root,text=rec[5],bg='red',width=button_width,height=10,borderwidth=3).place(x=900,y=p2_pos)
+chosenBin = -1
+def clicked():
+    if chosenBin >= 0:
+        giveawayPile = binAmount[chosenBin]
+        binAmount[chosenBin] = 0
+        if int(giveawayPile) <= 0:
+            messageCode = -1  # empty bin was chosen
+    i = 0
+    for element in binAmount:
+        binAmount[i] = binAmount[i]
+        if binAmount[i] < 10:
+            binAmount[i] = binAmount[i]
+        else:
+            binAmount[i] = binAmount[i]
+        i = i + 1
+    print(i)
 
-tkinter.Button(root,text=rec[7] ,bg='blue',width=button_width,height=10,borderwidth=3).place(x=150,y=p1_pos)
-tkinter.Button(root,text=rec[8] ,bg='blue',width=button_width,height=10,borderwidth=3).place(x=300,y=p1_pos)
-tkinter.Button(root,text=rec[9] ,bg='blue',width=button_width,height=10,borderwidth=3).place(x=450,y=p1_pos)
-tkinter.Button(root,text=rec[10],bg='blue',width=button_width,height=10,borderwidth=3).place(x=600,y=p1_pos)
-tkinter.Button(root,text=rec[11],bg='blue',width=button_width,height=10,borderwidth=3).place(x=750,y=p1_pos)
-tkinter.Button(root,text=rec[12],bg='blue',width=button_width,height=10,borderwidth=3).place(x=900,y=p1_pos)
 
-# red_player_layout(p2_pos)
-tkinter.Label(root,text='score \n {}'.format(rec[6]),bg='red',width=20,height=23).place(x=1055,y=0)
+
+
+tkinter.Button(root,text=binAmount[0],command=clicked, bg='red',width=button_width,height=10,borderwidth=3).place(x=150,y=p2_pos)
+tkinter.Button(root,text=binAmount[1],command=clicked,bg='red',width=button_width,height=10,borderwidth=3).place(x=300,y=p2_pos)
+tkinter.Button(root,text=binAmount[2],command=clicked,bg='red',width=button_width,height=10,borderwidth=3).place(x=450,y=p2_pos)
+tkinter.Button(root,text=binAmount[3],command=clicked,bg='red',width=button_width,height=10,borderwidth=3).place(x=600,y=p2_pos)
+tkinter.Button(root,text=binAmount[4],command=clicked,bg='red',width=button_width,height=10,borderwidth=3).place(x=750,y=p2_pos)
+tkinter.Button(root,text=binAmount[5],command=clicked,bg='red',width=button_width,height=10,borderwidth=3).place(x=900,y=p2_pos)
+tkinter.Label(root,text='score \n {}'.format(binAmount[6]),bg='red',width=20,height=23).place(x=1055,y=0)
+
+
+tkinter.Button(root,text=binAmount[7],command=clicked,bg='blue',width=button_width,height=10,borderwidth=3).place(x=150,y=p1_pos)
+tkinter.Button(root,text=binAmount[8] ,command=clicked,bg='blue',width=button_width,height=10,borderwidth=3).place(x=300,y=p1_pos)
+tkinter.Button(root,text=binAmount[9] ,command=clicked,bg='blue',width=button_width,height=10,borderwidth=3).place(x=450,y=p1_pos)
+tkinter.Button(root,text=binAmount[10],command=clicked,bg='blue',width=button_width,height=10,borderwidth=3).place(x=600,y=p1_pos)
+tkinter.Button(root,text=binAmount[11],command=clicked,bg='blue',width=button_width,height=10,borderwidth=3).place(x=750,y=p1_pos)
+tkinter.Button(root,text=binAmount[12],command=clicked,bg='blue',width=button_width,height=10,borderwidth=3).place(x=900,y=p1_pos)
+tkinter.Label(root,text='score \n {}'.format(binAmount[13]),bg='blue',width=18,height=23).place(x=0,y=0)
+
 
 
 root.mainloop()
