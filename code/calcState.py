38d2ddb0ec5fc573cee5d45cb9f4d6b69b8315df
hady993 <57845885+hady993@users.noreply.github.com>
2021-06-10 23:04:29 +0300

    Update calcState.py

--- code/calcState.py
+++ code/calcState.py
@@ -22,20 +22,20 @@ class AI:
             score = score -1        
         return temp
 
-    def get_diff_score(self):
+    def get_diff_score(self,bits):
         if not reversed:
-            return self.bits[6] - self.bits[13]
+            return bits[6] - bits[13]
         else:
-            return self.bits[13] - self.bits[6]
+            return bits[13] - bits[6]
     def possible_player_moves(self,player):
         if player==0:
-            for i, a in enumerate(self.bits[0:7]):
+            for i, a in enumerate(self.bits[0:6]):
                 if a > 0:
                     yield i
         else:
-             for i, a in enumerate(self.bits[8:]):
+             for i, a in enumerate(self.bits[7:13]):
                 if a > 0:
-                    yield i
+                    yield i+7
         
                 
     def find_all_moves(self,player=0):
@@ -44,30 +44,43 @@ class AI:
             all_moves.append([i,self.CalculateState(self.bits,i,player)])
         return all_moves  
 
-    def isGameEnd(self):
-        if(sum(self.bits[0:6]) == 0 or sum(self.bits[7:13]) == 0):
+    def isGameEnd(self,bits):
+        if(sum(bits[0:6]) == 0 or sum(bits[7:13]) == 0):
             return True
         return False
    
-    def mini_max(self,depth=2, maximizing_player=False):
-        if depth == 0 or self.isGameEnd():
-            return self.get_diff_score()
+    def mini_max(self,bits,depth=2, maximizing_player=True):
+        if depth == 0 or self.isGameEnd(bits):
+            return self.get_diff_score(bits)
         
         if maximizing_player:
             best_value = -1000
+            
             for move, board in self.find_all_moves(1):
-                val = self.mini_max(depth - 1, not maximizing_player)
-                best_value = max(best_value, val)
+                
+                val = self.mini_max(board,depth - 1, not maximizing_player)
+               # best_value = max(best_value, val)
+                index=0
+                if val>best_value:
+                   index=move
+                   best_value=val
                 #print(val)
                 #print(move)
-            return best_value
+                #print(board)
+            return index
         
         else:
             best_value = 1000
             for move, board in self.find_all_moves(1):
-                val = self.mini_max(depth - 1, not maximizing_player)
-                best_value = min(best_value, val)
-            return best_value
+                
+                val = self.mini_max(board,depth - 1, not maximizing_player)
+                #best_value = min(best_value, val)
+                if val<best_value:
+                   index=move
+                   best_value=val
+                #print(board)
+                
+            return index
 
 
 
@@ -138,8 +151,10 @@ def compute(x):
 
 
          
-bits=[0, 0, 0, 0, 0, 1, 5, 4, 4, 4, 4, 4, 4 , 10 ]
+#bits=[0, 0, 0, 0, 9, 1, 5, 4, 4, 4, 4, 4, 4 , 10 ]
+bits=[0, 0, 0, 0, 9, 1, 5, 0, 0, 0, 0, 9, 1 , 10 ]
 test = AI(bits)
-print(test.mini_max(1))
-#print(test.find_all_moves())
+#print(test.mini_max(bits))
+print(test.find_all_moves(1))
+
 
